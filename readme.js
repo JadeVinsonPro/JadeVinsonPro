0ed4a1290196f695b4cd1653f1bceae4c40229bf
Jade V <79988208+JadeVinsonPro@users.noreply.github.com>
2024-02-07 14:40:39 +0100

    Update readme.js

--- readme.js
+++ readme.js
@@ -11,8 +11,6 @@ module.exports = `
 
 - 📫 Reach me **jade.vinson.pro@gmail.com**
 
-- 📄 Know about my experiences [https://jadevinson.fr/stages-effectues.html](https://jadevinson.fr/stages-effectues.html)
-
 <h3 align="left">Connect with me:</h3>
 <p align="left">
 <a href="https://linkedin.com/in/jade-vinson" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="jade-vinson" height="30" width="40" /></a>
